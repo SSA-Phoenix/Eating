df44efa13927259cc294e443b1e3f17e3e5d09b6
javekk <periniraffaele@gmail.com>
2018-10-06 00:29:25 +0200

    add text rec

--- app/src/main/java/eu/ssaphoenix/eating/MainActivity.java
+++ app/src/main/java/eu/ssaphoenix/eating/MainActivity.java
@@ -1,13 +1,29 @@
 package eu.ssaphoenix.eating;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_OCR_CAPTURE = 9003;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onClick(View v) {
+        // launch Ocr capture activity.
+        Intent intent = new Intent(this, GetText.class);
+        intent.putExtra(GetText.AutoFocus, true);
+        intent.putExtra(GetText.UseFlash, false);
+
+        startActivity(intent);
+
+    }
+
 }
